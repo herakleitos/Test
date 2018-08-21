0e6902f927ebc4448c77fffe6d082d694b9ab923
Herakleitos <ni.lee@live.com>
2018-08-21 10:24:48 +0800

    add

---
+++ MVCTest/MVCTest/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTest.Models;
+namespace MVCTest.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "Hello World!";
+        }
+        public ActionResult GetView()
+        {
+            Employee e = new Employee();
+            e.FirstName = "姓氏";
+            e.LastName = "名字";
+            e.salary = 15000;
+            ViewData["Employee"] = e;
+            return View("MyView");
+        }
+    }
+}
