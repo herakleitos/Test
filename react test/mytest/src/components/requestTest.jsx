ba44a4e45fba80e51f16870d5453b98eb652976f
Herakleitos <ni.lee@live.com>
2018-09-05 17:55:16 +0800

    modify

--- react test/mytest/src/components/requestTest.jsx
+++ react test/mytest/src/components/requestTest.jsx
@@ -3,7 +3,8 @@ class requestTest extends Component{
     constructor(props){
         super(props);
         this.state={
-            text:''
+            text:'',
+            json:''
         }
     }
     getData(){
@@ -13,13 +14,25 @@ class requestTest extends Component{
                     text:data
                 });
             }
-        )
+        );
+        fetch('http://127.0.0.1:8081/json',{method:'GET'}).then(rest=>rest.json()).then(
+            data=>{
+                this.setState({
+                    json:data
+                });
+            }
+        );
     }
     componentWillMount(){
         this.getData();
     }
     render(){
-        return(<div>{this.state.text}</div>)
+        return(<div>
+            <div>{this.state.json.name}</div>
+            <div>{this.state.json.age}</div>
+            <div>{this.state.json.sex}</div>
+            <div>{this.state.text}</div>
+            </div>)
     }
 }
 export default requestTest
--- react test/nodeJsServer/myserver.js
+++ react test/nodeJsServer/myserver.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var app = express();
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-    });
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "X-Requested-With");
+res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+res.header("X-Powered-By",' 3.2.1')
+res.header("Content-Type", "application/json;charset=utf-8");
+next();
+});
+
 app.get('/', function (req, res) {
 res.send('This is test message!');
+})
+
+app.get('/json', function (req, res) { //添加的代码
+let myjson = {
+name : '张三',
+age : '36',
+sex : '男'
+}
+res.send(myjson);
 })
 var server = app.listen(8081, function () {
 var host = server.address().address
